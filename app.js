8fa31f3040358a381c4888c7720a7f660c6e7db6
Subutay <subutay@kali.kali.com>
2026-01-29 14:30:33 +0300

    Risk analizi fonksiyonunu yazdım

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.querySelector("#tarama").addEventListener("click", async () => {
 
   let url = getUrl(document.querySelector("#input").value.trim());
   let ip = await getIPFromDomain(url.hostname);
+  document.querySelector("#ip").textContent = ip;
   const domain = url.hostname.replace(/^www\./, "");
 
   
@@ -9,20 +10,134 @@ document.querySelector("#tarama").addEventListener("click", async () => {
   console.log(`Tam URL : ${url.href}\nİP : ${ip}\nDomain : ${domain}\nProtocol : ${url.protocol}`);
 
 
-  const httpsmi = isHTTPS(url);
-  const blacklisttemi = await checkBlackList(domain);
-  const domuinintarihi = await domainDate(domain);
-  const ulkebilgisi = await serverAndOrg(ip);
-  const tunnelmi = isTunnel(domain);
-  const httpheaderi = await httpHeaders(url); 
+  const cryptology= isHTTPS(url);
+  const isBlackList = await checkBlackList(url);
+  const domainD = await domainDate(domain);
+  const countryInfo = await serverAndOrg(ip);
+  const isTunnel = checkTunnel(domain);
+  const httpHeader = await httpHeaders(url); 
 
-  console.log("SSL :", httpsmi);
-  console.log("black List :",blacklisttemi)
-  console.log("domain kaç gün önce alındı :",domuinintarihi)
-  console.log("Barındaırma bilgisi :",ulkebilgisi)
-  console.log("tünelleme var mı : ",tunnelmi)
-  console.log("Http baslığı :",httpheaderi)
+  analysisfunc(cryptology, isBlackList, domainD, countryInfo, isTunnel, httpHeader);
+
 });
+
+// Analiz Fonksiyonu
+function analysisfunc(cryptology, isBlackList, domainD, countryInfo, isTunnel, httpHeader) {
+  let totalRisk = 0;
+  let malwareRisk = 0;
+  let phisingRisk = 0;
+
+
+  document.querySelector("#saglayıcı").textContent = `${countryInfo.ISP}`
+  document.querySelector("#ulke").textContent = `${countryInfo.countryName} ${countryInfo.countryCode}`
+ 
+
+  if (!httpHeader) {
+    console.log("sitede cors var");
+  } else {
+    totalRisk =- 5;
+    malwareRisk =-5;
+    phisingRisk =- 5;
+
+
+    const httpAnalysis = {
+      redirectRisk: headerJson.status >= 300 && headerJson.status < 400,
+      downloadRisk: (
+      contentType.includes("application/octet-stream") || 
+      contentType.includes("application/zip") || 
+      contentType.includes("application/x-msdownload") ||
+      headers.disposition?.includes("attachment")
+    ), 
+      hiddenServer: headerJson.headers.server === null,
+      corsLimited: headerJson.type !== "basic"
+    }
+
+    if (redirectRisk) {
+      totalRisk =+ 20;
+      malwareRisk =+ 10;
+      phisingRisk =+ 10;
+    } else {totalRisk =-5};
+
+    if (httpAnalysis.downloadRisk) {
+      totalRisk =+ 10;
+      malwareRisk =+ 60;
+      div.style.borderColor="#DC143C";
+      div.children[0].classList = "size-10 md:size-14 rounded-xl md:rounded-2xl bg-crimson/10 flex items-center justify-center mb-3 md:mb-4 border border-crimson/20"
+      div.children[0].children[0].classList = "material-symbols-outlined text-crimson text-2xl md:text-3xl" 
+      div.children[2].classList = "w-full md:w-auto px-2 md:px-4 py-1.5 rounded-full bg-crimson text-white text-[10px] md:text-xs font-black uppercase tracking-widest" 
+      div.children[2].textContent="Güvensiz";     
+    } else {
+      malwareRisk =- 20;
+      div.style.borderColor="";
+      div.children[0].classList = "size-10 md:size-14 rounded-xl md:rounded-2xl bg-primary/10 flex items-center justify-center mb-3 md:mb-4 border border-primary/20" 
+      div.children[0].children[0].classList = "material-symbols-outlined text-primary text-2xl md:text-3xl" 
+      div.children[2].classList = "w-full md:w-auto px-2 md:px-4 py-1.5 rounded-full bg-primary text-black text-[10px] md:text-xs font-black uppercase tracking-widest" 
+      div.children[2].textContent="Güvenli"; 
+    }
+  }
+
+  let div = document.querySelector("#domainyas")
+  if(domainD === null){
+    div.textContent = `Domain yası bulunamadı`;
+    div.classList = "text-base md:text-lg font-bold text-crimson"
+  } else if (domainD< 7) {
+    div.textContent = `${domainD} gün önce bu domain satın alındı`;
+    div.classList = "text-base md:text-lg font-bold text-crimson"
+  } else if (domainD < 30) {
+    div.textContent = `${Math.floor(domainD / 7)} Hafta önce bu domain satın alındı`;
+    div.classList = "text-base md:text-lg font-bold text-white"
+  } else if (domainD < 365) {
+    div.textContent = `${Math.floor(domainD / 30)} Ay önce bu domain satın alındı`;
+    div.classList = "text-base md:text-lg font-bold text-white"
+  } else {
+    div.textContent = `${Math.floor(domainD / 365)} Yıl önce bu domain satın alındı`;
+    div.classList = "text-base md:text-lg font-bold text-white"
+  } 
+
+  if (isTunnel) {
+    totalRisk =+ 50;
+    malwareRisk =+ 50;
+    phisingRisk =+ 50;
+    document.querySelector("#domainyas").textContent = "Tünelleme yapılıyor YÜKSEK RİSK";
+    document.querySelector("#domainyas").classList = "text-base md:text-lg font-bold text-crimson";
+  }
+
+  div =  document.querySelector("#ssl") 
+  if (cryptology) {
+    div.style.borderColor="";
+    div.children[0].classList = "size-10 md:size-14 rounded-xl md:rounded-2xl bg-primary/10 flex items-center justify-center mb-3 md:mb-4 border border-primary/20" 
+    div.children[0].children[0].classList = "material-symbols-outlined text-primary text-2xl md:text-3xl" 
+    div.children[2].classList = "w-full md:w-auto px-2 md:px-4 py-1.5 rounded-full bg-primary text-black text-[10px] md:text-xs font-black uppercase tracking-widest" 
+    div.children[2].textContent="Güvenli"; 
+  } else {
+    totalRisk =+ 20;
+    malwareRisk =+ 20;
+    phisingRisk =+ 20
+    div.style.borderColor="#DC143C";
+    div.children[0].classList = "size-10 md:size-14 rounded-xl md:rounded-2xl bg-crimson/10 flex items-center justify-center mb-3 md:mb-4 border border-crimson/20"
+    div.children[0].children[0].classList = "material-symbols-outlined text-crimson text-2xl md:text-3xl" 
+    div.children[2].classList = "w-full md:w-auto px-2 md:px-4 py-1.5 rounded-full bg-crimson text-white text-[10px] md:text-xs font-black uppercase tracking-widest" 
+    div.children[2].textContent="Güvensiz";
+  }
+
+  div = document.querySelector("#blacklist");
+  if (isBlackList) {
+    totalRisk = 100;
+    phisingRisk = 100;
+    div.style.borderColor="#DC143C";
+    div.children[0].classList = "size-10 md:size-14 rounded-xl md:rounded-2xl bg-crimson/10 flex items-center justify-center mb-3 md:mb-4 border border-crimson/20"
+    div.children[0].children[0].classList = "material-symbols-outlined text-crimson text-2xl md:text-3xl" 
+    div.children[2].classList = "w-full md:w-auto px-2 md:px-4 py-1.5 rounded-full bg-crimson text-white text-[10px] md:text-xs font-black uppercase tracking-widest" 
+    div.children[2].textContent="Güvensiz";
+  } else {
+    div.style.borderColor="";
+    div.children[0].classList = "size-10 md:size-14 rounded-xl md:rounded-2xl bg-primary/10 flex items-center justify-center mb-3 md:mb-4 border border-primary/20" 
+    div.children[0].children[0].classList = "material-symbols-outlined text-primary text-2xl md:text-3xl" 
+    div.children[2].classList = "w-full md:w-auto px-2 md:px-4 py-1.5 rounded-full bg-primary text-black text-[10px] md:text-xs font-black uppercase tracking-widest" 
+    div.children[2].textContent="Güvenli";
+  }
+
+}
 
 // URLyi alma fonksiyonu
 function getUrl(inputValue) {
@@ -32,7 +147,6 @@ function getUrl(inputValue) {
     }
     return new URL(inputValue);
   } catch (err) {
-      console.log(`Hata : ${err.message}`);
       alert(err.message)
   };
 };
@@ -43,19 +157,6 @@ function isHTTPS(url) {
     if(url.protocol === "http:") return false;
     alert("Bu bir web site linki değildir");
 }
-/*
-      const div =  document.querySelector("#ssl") 
-      div.style.borderColor="";
-      div.children[0].classList = "size-10 md:size-14 rounded-xl md:rounded-2xl bg-primary/10 flex items-center justify-center mb-3 md:mb-4 border border-primary/20" 
-      div.children[0].children[0].classList = "material-symbols-outlined text-primary text-2xl md:text-3xl" 
-      div.children[2].classList = "w-full md:w-auto px-2 md:px-4 py-1.5 rounded-full bg-primary text-black text-[10px] md:text-xs font-black uppercase tracking-widest" 
-      div.children[2].textContent="Güvenli"; */
-///////////////////
-/*      div.style.borderColor="#DC143C";
-      div.children[0].classList = "size-10 md:size-14 rounded-xl md:rounded-2xl bg-crimson/10 flex items-center justify-center mb-3 md:mb-4 border border-crimson/20"
-      div.children[0].children[0].classList = "material-symbols-outlined text-crimson text-2xl md:text-3xl" 
-      div.children[2].classList = "w-full md:w-auto px-2 md:px-4 py-1.5 rounded-full bg-crimson text-white text-[10px] md:text-xs font-black uppercase tracking-widest" 
-      div.children[2].textContent="Güvensiz"; */
 
 
 // IP adresini bulan fonksiyon
@@ -75,27 +176,13 @@ function isIP(ip) {
 
 
 // BlackList kontrolü
-async function checkBlackList(domain) {
-  let data = await fetch(`https://api.phishstats.info/api/phishing?_where=(url,like,${domain})`);
+async function checkBlackList(url) {
+  let data = await fetch(`https://api.phishstats.info/api/phishing?_where=(url,like,${url})`);
   data = await data.json()
   if (data[0]) return true;
   return false;
 }
-/*
-      const div = document.querySelector("#blacklist");
-      div.style.borderColor="#DC143C";
-      div.children[0].classList = "size-10 md:size-14 rounded-xl md:rounded-2xl bg-crimson/10 flex items-center justify-center mb-3 md:mb-4 border border-crimson/20"
-      div.children[0].children[0].classList = "material-symbols-outlined text-crimson text-2xl md:text-3xl" 
-      div.children[2].classList = "w-full md:w-auto px-2 md:px-4 py-1.5 rounded-full bg-crimson text-white text-[10px] md:text-xs font-black uppercase tracking-widest" 
-      div.children[2].textContent="Güvensiz";
-       
-      div.style.borderColor="";
-      div.children[0].classList = "size-10 md:size-14 rounded-xl md:rounded-2xl bg-primary/10 flex items-center justify-center mb-3 md:mb-4 border border-primary/20" 
-      div.children[0].children[0].classList = "material-symbols-outlined text-primary text-2xl md:text-3xl" 
-      div.children[2].classList = "w-full md:w-auto px-2 md:px-4 py-1.5 rounded-full bg-primary text-black text-[10px] md:text-xs font-black uppercase tracking-widest" 
-      div.children[2].textContent="Güvenli";
- */
-
+      
 // Domainin Satın Alındığı Tarih
 async function domainDate(domain) {
   try {
@@ -120,21 +207,6 @@ async function domainDate(domain) {
   }
 }
 
-/*
-  const div = document.querySelector("#domainyas")
-  div.classList = "text-base md:text-lg font-bold text-white"
-  if (day < 7) {
-    div.textContent = `${day} gün önce bu domain satın alındı`;
-    div.classList = "text-base md:text-lg font-bold text-crimson"
-  } else if (day < 30) {
-    div.textContent = `${Math.floor(day / 7)} Hafta önce bu domain satın alındı`;
-  } else if (day < 365) {
-    div.textContent = `${Math.floor(day / 30)} Ay önce bu domain satın alındı`;
-  } else {
-    div.textContent = `${Math.floor(day / 365)} Yıl önce bu domain satın alındı`;
-  } 
-*/
-
 // Sunucunun Konumu Ve Sağlayıcısı
 async function serverAndOrg(ip) {
   try {
@@ -146,15 +218,13 @@ async function serverAndOrg(ip) {
       countryCode: data.country_code,
       ISP: data.org
     };
-//    document.querySelector("#saglayıcı").textContent = `${data.org}`
-//    document.querySelector("#ulke").textContent = `${data.country_name} ${data.country_code}`
   } catch (err) {
     alert(err.message);
   }
 }
 
 //Tünelleme yapılıp yapılmadığını analiz eder
-function isTunnel(domain) {
+function checkTunnel(domain) {
   const tunnelServices = [
   "trycloudflare.com",
   "ngrok.io",
@@ -181,8 +251,6 @@ function isTunnel(domain) {
   return false;
 
 };
-//    document.querySelector("#domainyas").textContent = "Tünelleme yapılıyor YÜKSEK RİSK";
-//    document.querySelector("#domainyas").classList = "text-base md:text-lg font-bold text-crimson";
 
 
 // Header analiz
@@ -200,18 +268,15 @@ async function httpHeaders(url) {
         contentType: res.headers.get("content-type"),
         contentLength: res.headers.get("content-length"),
         location: res.headers.get("location"),
-        server: res.headers.get("server")
+        server: res.headers.get("server"),
+        disposition: res.headers.get("content-disposition"),
+        security: res.headers.get("strict-transport-security"),
+        poweredBy: res.headers.get("x-powered-by"),
+        cookie: res.headers.get("set-cookie")
       }
     };
   } catch (err) {
     return false;
   }
 }
-/*
-    const analysis = {
-      redirectRisk: headerJson.status >= 300 && headerJson.status < 400,
-      downloadRisk: headerJson.headers.contentType?.includes("application"),
-      hiddenServer: headerJson.headers.server === null,
-      corsLimited: headerJson.type !== "basic"
-    }
-*/
+
